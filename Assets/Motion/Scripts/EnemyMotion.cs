bf39a764b26bb7914557afe9048ec869242c62b0
DESKTOP-L38QT5L\vantan <tai-susi@ezweb.ne.jp>
2022-10-14 18:48:55 +0900

    [update]プレイヤーのアニメーションを関数で呼べるようにした。

--- Assets/Motion/Scripts/EnemyMotion.cs
+++ Assets/Motion/Scripts/EnemyMotion.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class EnemyMotion : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //関数で敵のアニメーションを呼び出せる
 }
