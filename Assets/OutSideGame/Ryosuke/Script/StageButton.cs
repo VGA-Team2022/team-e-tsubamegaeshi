115c18401f3488a606e9688eb564e1886bf26fd8
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-11-11 20:19:38 +0900

    Mainシーンに背景設定、Stage選択画面に背景を追加。 ボタンを押したときになるSEを追加。 バイブレーションの追加

--- Assets/OutSideGame/Ryosuke/Script/StageButton.cs
+++ Assets/OutSideGame/Ryosuke/Script/StageButton.cs
@@ -11,7 +11,10 @@ public class StageButton : MonoBehaviour
 
     public void OnClickStartButton()
     {
+        AudioManager.Instance.PlaySE("SE_RepelEnemy_DEMO_Ver.2_20221101");
         SceneManager.LoadScene("StageScene");
     }
+
+
 
 }
