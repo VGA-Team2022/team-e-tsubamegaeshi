6b4791fcd19b01befdb5134e275c6d32f7d3e3ed
ryousuke <83821881+Ryosuke004682@users.noreply.github.com>
2022-11-26 03:13:47 +0900

    スクリプトの最適化、フェイドアウト、フェイドイン追加、フェイド演出をインポート

---
+++ Assets/OutSideGame/Ryosuke/Script/StartButton.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    //ステージ遷移用スクリプト
+
+    
+
+
+
+}
