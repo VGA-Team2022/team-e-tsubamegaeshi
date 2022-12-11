80abf488eb124ee2adbd86e1e598140c4fce236d
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-12-11 20:22:18 +0900

    [update]途中

--- Assets/Script/LevelManager/StatusDataManager.cs
+++ Assets/Script/LevelManager/StatusDataManager.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatusDataManager : MonoBehaviour
+public class Data
 {
-    // Start is called before the first frame update
-    void Start()
+
+}
+
+public class StatusDataManager : Data
+{
+    public void Save()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Load()
     {
-        
+
     }
 }
