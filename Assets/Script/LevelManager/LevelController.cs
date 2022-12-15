821489b0eb66b55ec77676d8672a31b40c73abc5
TakaiKentarou <mmpm664@yahoo.co.jp>
2022-12-15 17:24:14 +0900

    [update]LevelController完成

--- Assets/Script/LevelManager/LevelController.cs
+++ Assets/Script/LevelManager/LevelController.cs
@@ -3,6 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
+/// 難易度のState
+/// </summary>
+public enum LevelState
+{
+    EASY = 0,
+    NORMAL = 1,
+    HARD = 2,
+}
+
+/// <summary>
 /// 難易度に応じて、プレイヤーとエネミー
 /// </summary>
 public class LevelController : MonoBehaviour
@@ -21,18 +31,9 @@ public class LevelController : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// 難易度のState
-    /// </summary>
-    public enum LevelState
-    {
-        EASY = 0,
-        NORMAL = 1,
-        HARD = 2,
-    }
-
-    [Header("難易度")]
-    public LevelState _levelState;
+    [Header("-------------------難易度の設定・ステータスを設定-------------------")]
+    [Header("難易度"), SerializeField]
+    LevelState _levelState;
 
     [Header("プレイヤー用ステータス")]
     [SerializeField]
@@ -50,23 +51,44 @@ public class LevelController : MonoBehaviour
     [SerializeField]
     private float _enemyKnockbackTime = 1f;
 
+
+    [Header("----------------------下記の項目は変更不可----------------------")]
     // プレイヤーのステータスを保存 
     // 1.Speed 2.KnockbackDistance 3.KnockbackTime
-    [System.NonSerialized] public float[] _easyPlayerStatus = new float[3];
-    [System.NonSerialized] public float[] _normalPlayerStatus = new float[3];
-    [System.NonSerialized] public float[] _hardPlayerStatus = new float[3];
+    [Tooltip("Easy用プレイヤーStatus")]
+    public float[] _easyPlayerStatus = new float[3];
+    [Tooltip("Normal用プレイヤーStatus")]
+    public float[] _normalPlayerStatus = new float[3];
+    [Tooltip("Hard用プレイヤーStatus")]
+    public float[] _hardPlayerStatus = new float[3];
 
     // エネミーのステータスを保存
     // 1.Speed 2.KnockbackDistance 3.KnockbackTime
-    [System.NonSerialized] public float[] _easyEnemyStatus = new float[3];
-    [System.NonSerialized] public float[] _normalEnemyStatus = new float[3];
-    [System.NonSerialized] public float[] _hardEnemyStatus = new float[3];
+    [Tooltip("Easy用エネミーStatus")]
+    public float[] _easyEnemyStatus = new float[3];
+    [Tooltip("Normal用エネミーStatus")]
+    public float[] _normalEnemyStatus = new float[3];
+    [Tooltip("Hard用エネミーStatus")]
+    public float[] _hardEnemyStatus = new float[3];
+
+    private LevelState _nowLevel;
+
+    public LevelState LevelState
+    {
+        get => _levelState;
+        set
+        {
+            _levelState = value;
+            ChangeLevelState(_levelState);
+        }
+    }
 
     /// <summary>
     /// インスペクター上でいじる際の変更を可視化
     /// </summary>
     private void OnValidate()
     {
+
         // プレイヤーステータス
         _playerSpeed = Mathf.Max(1, _playerSpeed);
         _playerKnockbackDistance = Mathf.Max(1, _playerKnockbackDistance);
@@ -77,25 +99,88 @@ public class LevelController : MonoBehaviour
         _enemyKnockbackDistance = Mathf.Max(1, _enemyKnockbackDistance);
         _enemyKnockbackTime = Mathf.Max(1, _enemyKnockbackTime);
 
+        StatusPutIn();
+    }
+
+    private void StatusPutIn()
+    {
         switch (_levelState)
         {
             case LevelState.EASY:
                 {
+                    if(_nowLevel != LevelState.EASY)
+                    {
+                        _playerSpeed = _easyPlayerStatus[0];
+                        _playerKnockbackDistance = _easyPlayerStatus[1];
+                        _playerKnockbackTime = _easyPlayerStatus[2];
 
+                        _enemySpeed = _easyEnemyStatus[0];
+                        _enemyKnockbackDistance = _easyEnemyStatus[1];
+                        _enemyKnockbackTime = _easyEnemyStatus[2];
+                    }
+
+                    _easyPlayerStatus[0] = _playerSpeed;
+                    _easyPlayerStatus[1] = _playerKnockbackDistance;
+                    _easyPlayerStatus[2] = _playerKnockbackTime;
+
+                    _easyEnemyStatus[0] = _enemySpeed;
+                    _easyEnemyStatus[1] = _enemyKnockbackDistance;
+                    _easyEnemyStatus[2] = _enemyKnockbackTime;
                 }
                 break;
 
             case LevelState.NORMAL:
                 {
+                    if (_nowLevel != LevelState.NORMAL)
+                    {
+                        _playerSpeed = _normalPlayerStatus[0];
+                        _playerKnockbackDistance = _normalPlayerStatus[1];
+                        _playerKnockbackTime = _normalPlayerStatus[2];
 
+                        _enemySpeed = _normalEnemyStatus[0];
+                        _enemyKnockbackDistance = _normalEnemyStatus[1];
+                        _enemyKnockbackTime = _normalEnemyStatus[2];
+                    }
+
+                    _normalPlayerStatus[0] = _playerSpeed;
+                    _normalPlayerStatus[1] = _playerKnockbackDistance;
+                    _normalPlayerStatus[2] = _playerKnockbackDistance;
+
+                    _normalEnemyStatus[0] = _enemySpeed;
+                    _normalEnemyStatus[1] = _enemyKnockbackDistance;
+                    _normalEnemyStatus[2] = _enemyKnockbackTime;
                 }
                 break;
 
             case LevelState.HARD:
                 {
+                    if (_nowLevel != LevelState.HARD)
+                    {
+                        _playerSpeed = _hardPlayerStatus[0];
+                        _playerKnockbackDistance = _hardPlayerStatus[1];
+                        _playerKnockbackTime = _hardPlayerStatus[2];
 
+                        _enemySpeed = _hardEnemyStatus[0];
+                        _enemyKnockbackDistance = _hardEnemyStatus[1];
+                        _enemyKnockbackTime = _hardEnemyStatus[2];
+                    }
+
+                    _hardPlayerStatus[0] = _playerSpeed;
+                    _hardPlayerStatus[1] = _playerKnockbackDistance;
+                    _hardPlayerStatus[2] = _enemyKnockbackTime;
+
+                    _hardEnemyStatus[0] = _enemySpeed;
+                    _hardEnemyStatus[1] = _enemyKnockbackDistance;
+                    _hardEnemyStatus[2] = _enemyKnockbackTime;
                 }
                 break;
         }
+
+        _nowLevel = LevelState;
+    }
+
+    private void ChangeLevelState(LevelState _level)
+    {
+        Debug.Log($"難易度が{_level}に変更された");
     }
 }
